039c7211e52d0e512ca7f67179f5529ad5e31f99
SantasScientists <43221425+SantasScientists@users.noreply.github.com>
2018-10-14 01:10:35 -0400

    revert back, sry achintya

--- python/predict_price.py
+++ python/predict_price.py
@@ -1,105 +1,63 @@
-import datetime as dt
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 13 15:59:34 2018
+
+@author: harry
+"""
+
+import pandas as pd
+import datetime as dt 
 import pandas_datareader.data as web
-import numpy as np    
+import numpy as np 
+import time as t
+import matplotlib.pyplot as plt
 
-list_last_point = []
 
-#def set_ticker(ticker, shares):
-#    global closing_set
-#    start = dt.datetime(2018, 1, 1)
-#    
-#    end_year = int(dt.datetime.today().strftime('%Y'))
-#    end_month = int(dt.datetime.today().strftime('%m'))
-#    end_day = int(dt.datetime.today().strftime('%d'))
-#    
-#    end = dt.datetime(end_year, end_month, end_day)
-#    
-#    df = web.DataReader(ticker, 'yahoo', start, end)
-#    data_set = df[['Close']]
-#    closing_set = data_set.values
-#    closing_set = [x*shares for x in closing_set]
+start = dt.datetime(2018,5,1)
+end = dt.datetime(year, month, day)
+df = web.DataReader('TSLA', 'yahoo', start, end)
+#print(df)
+df1 = df[['Close']]
+data = df1.values
+print(data)
+roc = []
 
-test = []
-total = []
-def total_volume(num_stocks):
-    global test
-    global total
-    year = int(dt.datetime.today().strftime('%Y'))
-    day = int(dt.datetime.today().strftime('%d'))
-    month = int(dt.datetime.today().strftime('%m'))
-    start = dt.datetime(2018,1,1)
-    end = dt.datetime(year, month, day)
-    for k in range(num_stocks):
-        a = input("Enter your stock")
-        b = int(input("Enter volume of stock"))
-        df = web.DataReader(a, 'yahoo', start, end)
-        df1 = df[['Close']]
-        data = df1.values
-        data = [x*b for x in data]
-        test.append(data)
-    total = [sum(x) for x in zip(*test)]
-    total = [float(x) for x in total]
-    
+for k in range(len(data)-1):
+    roc += list(np.log(data[k]/data[k+1]))
 
-def predict_price(days=1):
-    global list_last_point
-    rate_of_change = []
-    
-    for k in range(0, len(total)-1):
-        rate_of_change.append(np.log(total[k+1]/total[k]))
-    
-    S0 = float(total[-1])        
-    future_prices = [] 
-    
-    for i in range(1, days+1):
-        future_prices.append(float(total[-i]))
-    future_prices.reverse()
-    
-    avg = np.mean(rate_of_change)    
-    std_dev = np.std(rate_of_change)
-    variance = std_dev ** 2
-    drift = avg - (variance/2)
-            
-    for i in range(days):
-        z = np.random.uniform(-.999999999, .99999999)
-        S1 = (S0 * np.exp((drift+(std_dev*z))))
-        future_prices.append(S1)
+
+
+
+#%% avg, stdv, variance, drift
+
+#for j in range(len(roc)):
+#    total = 0
+#    total += roc[k]
+#    avg = total/len(roc)
+avg = np.mean(roc)
+variance = np.std(roc)**2
+standard_de = np.std(roc)
+drift = avg - (variance/2)
+
+
+#%%
+
+def equation_test(runs=1):
+    close=data[len(data)-1]
+    predicts = []
+    while runs>0:
+        new_close = float(close*np.exp(drift+standard_de*np.random.uniform(0.0001,1)))
+        runs -= 1
+        print(new_close)
+        predicts.append(new_close)
+        close = new_close       
+    return predicts
+
+a = range(len(data))
+plt.plot(a,data)
+for n in range(100):
+    b = range(len(data),len(data)+20)
+    plt.figure(1)
+    plt.plot(b, equation_test(20))
         
-        if i == days-1:
-            list_last_point.append(S1)
-            
-        S0 = S1    
-    return future_prices
-
-def plot_function(days=1):
-    plt.figure(1)
-    plt.clf()
-    for k in range(300):
-        plt.plot(predict_price(days))
     
-def plot_trend():
-    plt.figure(2)
-    plt.clf()
-    plt.plot([0]*len(list_last_point), list_last_point, "ks")
-
-def plot_histogram():
-    plt.figure(3)
-    plt.clf()
-    plt.hist(list_last_point, 11)
-    
-def plot_trendline(days=1):
-    val = np.mean(list_last_point)
-    plt.figure(1)
-    plt.plot([days-1, (days*2)-1], [total[-1], val], 'ks-', markersize=12, linewidth=5)
-    
-#def run_all(ticker, days, shares):
-#    set_ticker(ticker, shares)
-#    plot_function(days)
-#    plot_trend()
-#    plot_histogram()
-#    plot_trendline(days)
-#    plt.figure(1)
-#    plt.title("Stock: " + ticker + " over time", fontsize=26)
-#    plt.xlabel("Days", fontsize=20)
-#    plt.ylabel("Dollars ($)", fontsize=20)
